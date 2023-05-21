c7049f5d7b58b55a3e639e49c21638c7c3fb5191
Jesse <jessetreharne@gmail.com>
2023-05-22 00:12:28 +0200

    refactor, clean up dollars

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,60 @@
-use std::collections::{HashMap, VecDeque};
-use std::rc::{Rc, Weak};
+use std::collections::VecDeque;
 use std::cell::RefCell;
 
 static ITEMS: [Item; 6] = [
-    Item { name: "Mixed Fruit", val: 2.15 },
-    Item { name: "French Fries", val: 2.75 },
-    Item { name: "Side Salad", val: 3.35 },
-    Item { name: "Hot Wings", val: 3.55 },
-    Item { name: "Mozzarella Sticks", val: 4.20 },
-    Item { name: "Sampler Plate", val: 5.80 },
+    Item { name: "Mixed Fruit", val: 215 },
+    Item { name: "French Fries", val: 275 },
+    Item { name: "Side Salad", val: 335 },
+    Item { name: "Hot Wings", val: 355 },
+    Item { name: "Mozzarella Sticks", val: 420 },
+    Item { name: "Sampler Plate", val: 580 },
 ];
+
+
+type NodeIdx = usize;
+
+
+trait Container<T>: Extend<T> {
+    // a generic trait for a FIFO Queue or LIFO Stack
+    // so that we can make the search function 
+    // do either BFS or DFS
+    fn put(&mut self, val: T) -> ();
+    fn get(&mut self) -> Option<T>;
+}
+
+
+type Stack<T> = Vec<T>;
+type Queue<T> = VecDeque<T>;
+
+
+impl <T>Container<T> for Stack<T> {
+    fn put(&mut self, val: T) -> () {
+        self.push(val);
+    }
+    fn get(&mut self) -> Option<T> {
+        self.pop()
+    }
+}
+
+
+impl <T>Container<T> for Queue<T> {
+    fn put(&mut self, val: T) -> () {
+        self.push_back(val);
+    }
+    fn get(&mut self) -> Option<T> {
+        self.pop_front()
+    }
+}
 
 
 struct Tree {
     nodes: RefCell<Vec<Node>>
 }
 
+
 impl Tree {
     fn new() -> Self {
-        let root_node = Node { item: None, total: 0.0, parent: None };
+        let root_node = Node { item: None, total: 0, parent: None };
         Tree { nodes: RefCell::new(vec![root_node]) }
     }
 
@@ -46,7 +82,7 @@ impl Tree {
         child_indexes.into_iter().collect()
     }
 
-    fn visit(&self, idx: NodeIdx, target: f32) -> Option<Vec<&Item>> {
+    fn visit(&self, idx: NodeIdx, target: Dollars) -> Option<Vec<&Item>> {
         let nodes = self.nodes.borrow();
         let mut node = &nodes[idx];
         
@@ -61,7 +97,7 @@ impl Tree {
         Some(items)
     }
 
-    fn search<C>(&mut self, target: f32, mut container: C) -> Option<Vec<&Item>> 
+    fn search<C>(&mut self, target: Dollars, mut container: C) -> Option<Vec<&Item>> 
     where C: Container<NodeIdx> {
         container.put(0);
         while let Some(node_idx) = container.get() {
@@ -73,56 +109,42 @@ impl Tree {
         };
         None
     }
+
+    fn bfs(&mut self, target: Dollars) -> Option<Vec<&Item>> {
+        self.search(target, Queue::new())
+    }
+
+    fn dfs(&mut self, target: Dollars) -> Option<Vec<&Item>> {
+        self.search(target, Stack::new())
+    }
 }
+
 
 #[derive(Debug)]
 struct Item {
     name: &'static str,
-    val: f32,
+    val: Dollars,
 }
 
-type NodeIdx = usize;
 
 struct Node {
     item: Option<&'static Item>,
-    total: f32,
+    total: Dollars,
     parent: Option<NodeIdx>,
 }
 
-trait Container<T>: Extend<T> {
-    // a generic type for a FIFO Queue or LIFO Stack
-    // so that we can make the search function 
-    // do either BFS or DFS
-    fn put(&mut self, val: T) -> ();
-    fn get(&mut self) -> Option<T>;
+
+type Dollars = i32;
+fn dollars(val: f32) -> Dollars {
+    (val * 100.0).round() as Dollars
 }
-
-type Stack<T> = Vec<T>;
-type Queue<T> = VecDeque<T>;
-
-impl <T>Container<T> for Stack<T> {
-    fn put(&mut self, val: T) -> () {
-        self.push(val);
-    }
-    fn get(&mut self) -> Option<T> {
-        self.pop()
-    }
-}
-
-impl <T>Container<T> for Queue<T> {
-    fn put(&mut self, val: T) -> () {
-        self.push_back(val);
-    }
-    fn get(&mut self) -> Option<T> {
-        self.pop_front()
-    }
-}
-
 
 fn main() {
-
     let mut tree = Tree::new();
-    let val = tree.search(15.05, Queue::new());
+    // let target = 3*275 + 2*335 + 4*580 + 3*420 ;
+    let target = dollars(42.35);
+    // let target = 15.05;
+    // let target = 1505;
+    let val = tree.bfs(target);
     println!("{:?}", val);
-
 }
